1ecc134c80b29ae281353070cc878bd73bde9722
A. Legayada <tonilegayada@gmail.com>
2025-02-19 19:23:32 +0800

    spinner addition

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,18 +1,24 @@
-import ClipLoader from 'react-spinners/ClipLoader';
+import PropTypes from 'prop-types'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 const override = {
   display: 'block',
   margin: '100px auto',
-};
+}
 
-const Spinner = ({ loading }) => {
+const Spinner = ({ loading = true, color = '#4338ca' }) => {
   return (
     <ClipLoader
-      color='#4338ca'
+      color={color}
       loading={loading}
       cssOverride={override}
       size={150}
     />
-  );
-};
-export default Spinner;
+  )
+}
+export default Spinner
+
+Spinner.PropTypes = {
+  loading: PropTypes.bool,
+  color: PropTypes.string,
+}
---
+++ src/stories/Spinner.stories.jsx
@@ -0,0 +1,34 @@
+import Spinner from '../components/Spinner'
+
+export default {
+  title: 'Lab2/Spinner',
+  component: Spinner,
+  tags: ['autodocs'],
+  argTypes: {
+    mode: {
+      options: ['#4338c', '#36454F', '#DAF7A6'],
+      control: { type: 'select' },
+    },
+  },
+}
+
+// #1
+export const Default = {
+  args: {
+    color: '#4338c',
+  },
+}
+
+// #2
+export const Dark = {
+  args: {
+    color: '#36454F',
+  },
+}
+
+// #3
+export const Colorful = {
+  args: {
+    color: '#DAF7A6',
+  },
+}
